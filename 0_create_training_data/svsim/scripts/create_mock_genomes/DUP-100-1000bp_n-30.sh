8b963669c21026a0e18acf302cc54f4041376c47
kyle <kyle.lesack@gmail.com>
2020-02-10 17:37:59 -0700

    added scripts for creating a mock genome in svsim

---
+++ 0_create_training_data/svsim/scripts/create_mock_genomes/DUP-100-1000bp_n-30.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=svsim_DUP-100-1000bp_n-30
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=single,lattice
+#SBATCH --time=01:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=kyle.lesack@gmail.com
+#SBATCH -o svsim_DUP-100-1000bp_n-30.%j.out
+#SBATCH -e svsim_DUP-100-1000bp_n-30.%j.err
+
+source ~/.bashrc
+
+conda activate SVsim
+
+REFERENCE=/home/kyle.lesack1/project_files/reference_genome/c_elegans.PRJNA13758.WS263.genomic.fa
+PROFILE=/home/kyle.lesack1/project_files/repos/sv_pipeline/0_create_training_data/svsim/profiles/DUP-100-1000bp.txt
+OUTPUT=/home/kyle.lesack1/project_files/repos/sv_pipeline/0_create_training_data/svsim/simulated_genomes/DUP-100-1000bp_n-30
+
+mkdir -p $OUTPUT
+
+python /home/kyle.lesack1/bin/SVsim-master/SVsim -i $PROFILE -r $REFERENCE -o $OUTPUT/SVSIM_DUP-100-1000bp_n-30 -W -n 30 -d
+mv /home/kyle.lesack1/project_files/repos/sv_pipeline/0_create_training_data/svsim/simulated_genomes/DUP-100-1000bp_n-30/SVSIM_DUP-100-1000bp_n-30.fasta /home/kyle.lesack1/project_files/repos/sv_pipeline/0_create_training_data/svsim/simulated_genomes/DUP-100-1000bp_n-30/SVSIM_DUP-100-1000bp_n-30.fa
+
+
+conda deactivate
+
+
