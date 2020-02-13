fee7911884fbd0b98873fd7b105f906daa03609d
kyle.lesack@gmail.com <kyle.lesack@gmail.com>
2020-02-12 22:42:58 -0700

    The assigned call_id variable depends on number of rows in bedpe file. This was needed because sv types with < 10 entries don't have a preceeding 0 in their name

--- 0_create_training_data/svsim/scripts/make_S0_vcf/svsim2vcf.py
+++ 0_create_training_data/svsim/scripts/make_S0_vcf/svsim2vcf.py
@@ -14,7 +14,7 @@ import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="input BED file (xxx.bedpe)")
-parser.add_argument("sample", help="sample name, usually xxx part of xxx.event")
+parser.add_argument("sample_name", help="sample name, usually xxx part of xxx.event")
 
 args = parser.parse_args()
 
@@ -24,14 +24,14 @@ today_is_the_greatest = (str(date.year) + "-" + str(date.month) + "-" + str(date
 # Read VCF Header file
 with open('VCF_Header.txt', 'r') as header_file :
 	header_data = header_file.read()
-	
+
 # Replace the target string
 header_data = header_data.replace('DATE', today_is_the_greatest) # Write the correct date in vcf header
 
 REF_ALLELE = "N" # VCF files have a reference allele field, I don't think that SVE needs this. I don't want to find it in the FASTA file.
 VARIANT_QUAL = "." # Simulated data, so a "." (unknown quality) is used
 VARIANT_FILTER = "PASS" # Simulated data, so PASS is used for filter field
-sample_name = args.sample
+sample_name = args.sample_name
 
 
 print(header_data, end='')
@@ -48,16 +48,28 @@ print(header_data, end='')
 
 with open(args.input) as fd:
 	rd = csv.reader(fd, delimiter="\t", quotechar='"')
+	row_count = sum(1 for row in rd) 
+
+with open(args.input) as fd:
+	rd = csv.reader(fd, delimiter="\t", quotechar='"')
+	row_count = sum(1 for row in rd) 
+
+with open(args.input) as fd:
+	rd = csv.reader(fd, delimiter="\t", quotechar='"')
 	for row in rd:
 		chromosome = row[0]
 		start_coord = row[2]
 		end_coord = row[4]
 		sv_length = int(end_coord) - int(start_coord)
 		variant_type = row[6][0:3]
-		call_id = row[6][0:5]
+		if row_count < 10:
+			call_id = row[6][0:4] # If the bedpe file has less than 10 variants of a given type, one digit will be used to number the variant name (e.g., DEL1 NOT DEL01)
+		else:
+			call_id = row[6][0:5]
 		variant_id = "SVSIM_" + call_id + "_" + str(sv_length) + "bp"
 		alt_allele = "<" + variant_type + ">" # ALT field is used to store the variant type in the VCF file
 		variant_info = "DBVARID;" + "CALLID=" + variant_id + ";" + "SVTYPE=" + variant_type + ";" + "EXPERIMENT=1;" + "SAMPLE=" + sample_name + ";" + "END=" + end_coord + ";" + "REGION=NA"
-		#print(variant_info)		
+		#print(variant_info)
 		vcf_row = chromosome + "\t" + start_coord + "\t" + variant_id + "\t" + REF_ALLELE + "\t" + alt_allele + "\t" + VARIANT_QUAL + "\t" + VARIANT_FILTER + "\t" + variant_info
 		print(vcf_row)
+
