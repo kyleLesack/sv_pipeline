46fd2740f2c312c9dd9aeef64bb5d65be192927f
kyle.lesack@gmail.com <kyle.lesack@gmail.com>
2020-02-19 21:28:49 -0700

    creates different output directories for each job

--- 0_create_training_data/svsim/scripts/create_slurm_scripts/create_slurm_from_svsim_profiles.py
+++ 0_create_training_data/svsim/scripts/create_slurm_scripts/create_slurm_from_svsim_profiles.py
@@ -14,8 +14,9 @@ parser.add_argument("output_dir", help="Directory to export the slurm files to")
 parser.add_argument("sv_type", help="Variant type. Must be DEL, DUP, INS, or INV")
 args = parser.parse_args()
 
-def make_slurm_file(profile_file,job_name,array_id_number,template_file):
+def make_slurm_file(profile_file,job_name,array_id_number,template_file,output_dir):
 	with open(template_file, 'r') as file :
+		output_dir=output_dir+job_name+"/"
 		filedata = file.read()
 		filedata = filedata.replace('REFERENCE_FILE', REFERENCE_FILE)
 		filedata = filedata.replace('PROFILE_FILE', profile_file)
@@ -28,14 +29,14 @@ def make_slurm_file(profile_file,job_name,array_id_number,template_file):
 
 
 		
-def get_profile_files(template_file):
+def get_profile_files(template_file,output_dir):
 	array_id_number = 1 # each job in a slurm array is assigned a number; slurm array submission script will call svsim sh scripts using this number 
 	for file in os.listdir(args.input_dir):
 		if file.endswith(".txt"):
 			profile_file= args.input_dir + file
 			base=os.path.basename(file)
 			job_name = os.path.splitext(base)[0] # Job name is based on the the profile file name, excluding the extension
-			make_slurm_file(profile_file,job_name,array_id_number,template_file)
+			make_slurm_file(profile_file,job_name,array_id_number,template_file,output_dir)
 			array_id_number += 1
 
 
@@ -44,7 +45,7 @@ if args.sv_type=="DEL":
 	output_dir=OUTPUT_DIR_PREFIX + "deletions/"
 	template_file = "./DEL_template.txt"
 	if os.path.isdir(args.output_dir):
-		get_profile_files(template_file)
+		get_profile_files(template_file,output_dir)
 	else:
 		print("output path not found")
 
@@ -53,7 +54,7 @@ elif args.sv_type=="DUP":
 	template_file = "./DUP_template.txt"
 
 	if os.path.isdir(args.output_dir):
-		get_profile_files(template_file)
+		get_profile_files(template_file,output_dir)
 	else:
 		print("output path not found")
 
@@ -62,7 +63,7 @@ elif args.sv_type=="INS":
 	template_file = "./INS_template.txt"
 
 	if os.path.isdir(args.output_dir):
-		get_profile_files(template_file)
+		get_profile_files(template_file,output_dir)
 	else:
 		print("output path not found")
 
@@ -70,7 +71,7 @@ elif args.sv_type=="INV":
 	output_dir=OUTPUT_DIR_PREFIX + "inversions/"
 	template_file = "./INV_template.txt"
 	if os.path.isdir(args.output_dir):
-		get_profile_files(template_file)
+		get_profile_files(template_file,output_dir)
 	else:
 		print("output path not found")
 
